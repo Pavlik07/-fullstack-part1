02586bbdfcbb03d859ab143cdb79f3b966e22dd1
Pavlik07 <pavelmos0707@gmail.com>
2020-06-19 15:20:17 +0300

    something

---
+++ courseinfo/src/index.js
@@ -0,0 +1,56 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+      ]
+  }  
+
+  
+  const Header = () => { 
+    return (
+    <div>
+      <h1> {course.name} </h1>
+    </div>)
+  }
+
+  const Content = () => {
+    return (
+    <div>
+      <p> {course.parts[0].name} {course.parts[0].exercises} </p>
+      <p> {course.parts[1].name} {course.parts[1].exercises} </p>
+      <p> {course.parts[2].name} {course.parts[2].exercises} </p>
+    </div>)
+  }
+  
+  const Total = () => {
+    return (
+    <div>
+      <p>Number of exercises {course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises}</p>
+    </div>)
+  }
+
+  return (
+    <div>
+      <Header />
+      <Content />
+      <Total />
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
